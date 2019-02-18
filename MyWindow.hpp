6eb058971ee6b1be7d92de0c6e4d16a90e4cbd80
apatel435 <apatel435@gatech.edu>
2019-02-18 15:10:14 -0500

    Made some changes to comply with DART 6.7.2

--- MyWindow.hpp
+++ MyWindow.hpp
@@ -38,7 +38,7 @@
 #include "Controller.hpp"
 
 /// \brief class MyWindow
-class MyWindow : public dart::gui::SimWindow
+class MyWindow : public dart::gui::glut::SimWindow
 {
 public:
   /// \brief Default constructor
